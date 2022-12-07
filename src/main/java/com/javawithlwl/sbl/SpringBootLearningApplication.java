3d2205ec1346c914e59375b0516fa6ad3f5c9462
javawithlwl <javawithlwl@gmail.com>
2022-12-07 10:22:29 +0530

    Added Greetings api

--- src/main/java/com/javawithlwl/sbl/SpringBootLearningApplication.java
+++ src/main/java/com/javawithlwl/sbl/SpringBootLearningApplication.java
@@ -2,10 +2,17 @@ package com.javawithlwl.sbl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootLearningApplication {
 
+	@GetMapping("/")
+	public String greetings(){
+		return "Welcome to Docker World";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 	}
